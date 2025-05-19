fc1787c68161c563d3cf8a4c585807f301ad687d
Callum Forsyth <c-forsyth02@sky.com>
2025-05-19 11:19:16 +0100

    save

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -8,31 +8,25 @@ document.addEventListener("DOMContentLoaded", function() {
                 <li><a href="GamesHome.html">Games</a></li>
             </ul>
         </div>
-        <div>
-            <dl id="social">
-                <!--List-->
-                <dd class="leftList">
-                    <h2>
-                        <img src="images/LinkedIn/LI-In-Bug.png" width="35" height="30" alt="" />
-                        <a href="https://www.linkedin.com/feed/">LinkedIn</a>
-                    </h2>
-                    <h2>
-                        <img src="images/GitHub/github-mark.png" width="30" height="30" alt="" />
-                        <a href="https://github.com/Ghosthi1">GitHub</a>
-                    </h2>
-                </dd>
-                <!--Creates a parallel list-->
-                <dd class="rightList">
-                    <h2>
-                        <img src="images/ItchIo/Itch.svg" width="35" height="30" alt="" />
-                        <a href="https://ghosthi.itch.io">itch.io</a>
-                    </h2>
-                    <h2>
-                        <img src="images/Misc/Mail.png" width="35" height="35" alt="" />
-                        <a href="mailto:c-forsyth02@sky.com">Email</a>
-                    </h2>
-                </dd>
-            </dl>
+        <div class="menuLinks" id="menuLinks">
+            <dd class="leftList">
+                <h2>
+                    <img src="images/LinkedIn/LI-In-Bug.png" width="35" height="30" alt="" />
+                    <a href="https://www.linkedin.com/feed/"></a>
+                </h2>
+                <h2>
+                    <img src="images/GitHub/github-mark.png" width="30" height="30" alt="" />
+                    <a href="https://github.com/Ghosthi1"></a>
+                </h2>
+                <h2>
+                    <img src="images/ItchIo/Itch.svg" width="35" height="30" alt="" />
+                    <a href="https://ghosthi.itch.io"></a>
+                </h2>
+                <h2>
+                    <img src="images/Misc/Mail.png" width="35" height="35" alt="" />
+                    <a href="mailto:c-forsyth02@sky.com"></a>
+                </h2>
+            </dd>
         </div>
     `;
     document.getElementById("menu").innerHTML = menuHTML;
